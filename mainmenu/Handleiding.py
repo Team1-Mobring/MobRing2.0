827cd3076a528fa4b6bc94de2a797eafbd831c87
Enny <theennyk@gmail.com>
2019-12-09 18:20:40 +0100

    Cleaned up Handleiding.py

--- mainmenu/Handleiding.py
+++ mainmenu/Handleiding.py
@@ -1,12 +1,8 @@
 def setup():
     background(0)
-    s = "Mobring!"
-    d = "the Dice war!"
-    
     fill(255)
     textSize(100)
-    text(s, 500, 400)
-    text(d, 500, 550)
+    text("Mobring!\nthe Dice War!", 500, 400)
 
 def draw():
     pass
