e6d26d9bca616137ac1fb37e01cb0d84945469c0
Almina38 <57941230+Almina38@users.noreply.github.com>
2019-12-16 22:21:30 +0100

    Update Handleiding.py

--- mainmenu/Handleiding.py
+++ mainmenu/Handleiding.py
@@ -1,12 +1,13 @@
 import functions
 
 def setup():
-    global background_image, goal_image, setup_image, rules_image, start_image, \
+    global background_image, goal_image, setup_image, setup1_image, rules_image, start_image, \
     course_image, course2_image
     
     background_image = loadImage("ManualMenu.png")
     goal_image = loadImage("goal.png")
     setup_image = loadImage("setup.png")
+    setup2_image = loadImage("setup2.png")
     rules_image = loadImage("rules.png")
     start_image = loadImage("start.png")
     course_image = loadImage("course.png")
@@ -15,3 +16,23 @@ def setup():
 def draw():
     image(background_image, 0, 0)
     
+def text1():
+    image(goal_image, 0, 0)
+    
+def text2():
+    image(setup_image, 0, 0)
+    
+def text3():
+    image(setup2_image, 0, 0)
+    
+def text4():
+    image(rules_image, 0, 0)
+    
+def text5():
+    image(start_image, 0, 0)
+    
+def text6():
+    image(course_image, 0, 0)
+    
+def text7():
+    image(course2_image, 0, 0)
