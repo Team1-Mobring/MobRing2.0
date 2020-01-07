224d7bf571e9cfe49763f3ff2f6f8328158b9e7a
Almina38 <57941230+Almina38@users.noreply.github.com>
2020-01-07 09:42:56 +0100

    Update Handleiding.py

--- mainmenu/Handleiding.py
+++ mainmenu/Handleiding.py
@@ -14,6 +14,12 @@ def setup():
     course2_image = loadImage("course2.png")
     highlightMenu_1 = loadImage("MainMenuHighlight.png")
     handleiding_page = 1
+    traps_image = loadImage("traps.png")
+    origins_image = loadImage("origins.png")
+    classes_image = loadImage("classes.png")
+    mobsters_image = loadImage("mobsters.png")
+    jobs_image = loadImage("jobs.png")
+    
     
 def draw():
     global handleiding_page, highlightMenu_1
@@ -42,6 +48,22 @@ def draw():
     
     if functions.isMouseWithinSpace2(TutorialBot.main_menu_x, TutorialBot.main_menu_y, 265, 90):
         image(highlightMenu_1, 0, 0)
+        
+    if handleiding_page == 8:
+        image(traps_image, 15, -28)
+        
+    if handleiding_page == 9:
+        image(origins_image, 15, -28)
+        
+    if handleiding_page == 10:
+        image(classes_image, 15, -28)
+        
+    if handleiding_page == 11:
+        image(mobsters_image, 15, -28)
+        
+    if handleiding_page == 12:
+        image(jobs_image, 15, -28)
+        
         
 # def text1():
 #     image(goal_image, 0, 0)
